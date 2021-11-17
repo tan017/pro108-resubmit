2fec6daccb66252bcb8f8a92f65086e0908101ff
Tanmay Hiremath <81285857+tan017@users.noreply.github.com>
2021-11-17 23:05:22 +0530

    Update bellCurve.py

--- bellCurve.py
+++ bellCurve.py
@@ -3,4 +3,5 @@ import plotly.figure_factory as ff
 import csv 
 df=pd.read_csv("datapro108.csv")
 fig=ff.create_distplot([df["Height(Inches)"].tolist()],["Height"],show_hist=False)
+fig = ff.create_distplot([df["Avg Rating"].tolist()], ["Avg Rating"])
 fig.show()
